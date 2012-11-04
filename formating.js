bc0e1be746c438d8da3dbdf632bb4123362b951c
Neeraj Khandelwal <nk.zscorpion@gmail.com>
2012-11-05 03:39:05 +0530

    Wysiwyg editors with basic features.

---
+++ formating.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+	this.getElementById('formatted-text').contentDocument.designMode = "on";
+	document.getElementById('formatted-text').contentDocument.getElementsByTagName('body')[0].style.fontSize = '13px';
+	document.getElementById('formatted-text').contentDocument.getElementsByTagName('body')[0].style.fontFamily = 'Helvetica';
+	var val = null;
+	
+	$('button').click(function(){
+		var format = this.id;
+		var val = window.val;
+		
+		var isActive = $(this).hasClass('active');
+		if(isActive){
+			$(this).removeClass('active');
+		}
+		else{
+			$(this).addClass('active');
+		}
+		
+		if(format == 'formatblock'){
+			if(val == '<p>')
+				val = '<pre>';
+			else if(val == '<pre>')
+				val = '<p>';
+			else
+				val = '<pre>';
+		}
+		else if(format == 'InsertImage'){
+		//image upload from hardisk missing.
+			var val = prompt("URL:");
+			$(this).removeClass('active');
+		}
+		window.val = val;
+		document.getElementById("formatted-text").contentWindow.document.execCommand(format, false, val);		
+	});
+	
+	$('#done').live('click', function(){
+		getUserText('formatted-text');
+	});
+	
+});
+
+function getUserText(id){
+	var formatingArea =  document.getElementById(id);
+	var formatted_text = formatingArea.contentDocument.getElementsByTagName('body')[0].innerHTML;
+	//htmlentities of php function for html safe.
+	$('.text-written').html(formatted_text);
+}
